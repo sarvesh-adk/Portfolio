2b2f4e699e7606ee9f08cae9200c74ef277ea38c
Sarvesh-adk <sarbeswar.adk@gmail.com>
2024-11-03 22:27:22 +0530

    changes

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu () {
+  const menu = document.querySelector('.menu-links')
+  const icon = document.querySelector('.hamburger-icon')
+  menu.classList.toggle('open')
+  icon.classList.toggle('open')
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const logo = document.querySelector('.logo')
+  logo.addEventListener('click', function () {
+    document.body.classList.toggle('dark-theme')
+    imgSrc()
+  })
+})
+
+function imgSrc () {
+  const expIcon = document.getElementById('experience-icon')
+  const eduIcon = document.getElementById('education-icon')
+  const checkIcon = document.getElementsByClassName('check-icon')
+
+  if (document.body.classList.contains('dark-theme')) {
+    expIcon.src = './assets/experience-dark.png'
+  } else {
+    expIcon.src = './assets/experience-light.png'
   }
-  document.addEventListener('DOMContentLoaded', function() {
-    const logo = document.querySelector('.logo');
-    logo.addEventListener('click', function() {
-      document.body.classList.toggle('dark-theme');
-    });
-  });
+}
