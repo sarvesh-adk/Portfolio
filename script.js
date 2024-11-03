ca60a247d3bc8478901dad4655bc95bd70de844b
Prakash Kumar <prakashkumar01031985@gmail.com>
2024-11-03 21:46:34 +0530

    gdrdj

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-  document.addEventListener('DOMContentLoaded', function() {
-    const logo = document.querySelector('.logo');
-    logo.addEventListener('click', function() {
-      document.body.classList.toggle('dark-theme');
-    });
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const logo = document.querySelector('.logo');
+  logo.addEventListener('click', function () {
+    document.body.classList.toggle('dark-theme');
+    imgSrc()
   });
+});
+
+function imgSrc() {
+  var expIcon = document.getElementById('experience-icon')
+  var eduIcon = document.getElementById('education-icon')
+  var checkIcon = document.getElementsByClassName('check-icon')
+  
+  if (document.body.classList.contains('dark-theme')) {
+    expIcon.src = './assets/experience-dark.png'
+  } else {
+    expIcon.src = './assets/experience-light.png'
+    
+  }
+}
