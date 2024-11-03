43ef5a11219c21d9d257db83aacf2b18177cfe12
Sarvesh-adk <sarbeswar.adk@gmail.com>
2024-11-03 11:31:39 +0530

    darktheme

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+  document.addEventListener('DOMContentLoaded', function() {
+    const logo = document.querySelector('.logo');
+    logo.addEventListener('click', function() {
+      document.body.classList.toggle('dark-theme');
+    });
+  });
