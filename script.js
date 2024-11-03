cde9387983c50ddacf47f0562e78fe1daff17305
Prakash Kumar <prakashkumar01031985@gmail.com>
2024-11-03 22:11:17 +0530

    hello

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+function toggleMenu () {
+  const menu = document.querySelector('.menu-links')
+  const icon = document.querySelector('.hamburger-icon')
+  menu.classList.toggle('open')
+  icon.classList.toggle('open')
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  const logo = document.querySelector('.logo');
+  const logo = document.querySelector('.logo')
   logo.addEventListener('click', function () {
-    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('dark-theme')
     imgSrc()
-  });
-});
+  })
+})
 
-function imgSrc() {
-  var expIcon = document.getElementById('experience-icon')
-  var eduIcon = document.getElementById('education-icon')
-  var checkIcon = document.getElementsByClassName('check-icon')
-  
+function imgSrc () {
+  let expIcon = document.getElementById('experience-icon')
+  let eduIcon = document.getElementById('education-icon')
+  let checkIcon = document.getElementsByClassName('check-icon')
+
   if (document.body.classList.contains('dark-theme')) {
     expIcon.src = './assets/experience-dark.png'
   } else {
     expIcon.src = './assets/experience-light.png'
-    
   }
 }
